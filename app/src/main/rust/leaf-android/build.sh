78e2bbe6fed7676dab3edaa959d35961f46ddce4
Vic <hiyunwei@gmail.com>
2024-04-11 14:10:41 +0800

    Update build script and JNI interface.

--- app/src/main/rust/leaf-android/build.sh
+++ app/src/main/rust/leaf-android/build.sh
@@ -1,56 +1,51 @@
 #!/bin/bash
 
-mode=debug
-
-if [ "$1" = "release" ]; then
-	mode=release
-fi
-
 BASE=`dirname "$0"`
 HOST_OS=`uname -s | tr "[:upper:]" "[:lower:]"`
 HOST_ARCH=`uname -m | tr "[:upper:]" "[:lower:]"`
 
+export PATH="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin/":$PATH
 android_tools="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin"
 api=26
 
-for target in x86_64-linux-android aarch64-linux-android; do
+for target in x86_64-linux-android aarch64-linux-android armv7-linux-androideabi i686-linux-android; do
     case $target in
         'x86_64-linux-android')
             export CC_x86_64_linux_android="$android_tools/${target}${api}-clang"
-            export AR_x86_64_linux_android="$android_tools/${target}-ar"
-            export CARGO_TARGET_X86_64_LINUX_ANDROID_AR="$android_tools/$target-ar"
+            export AR_x86_64_linux_android="$android_tools/llvm-ar"
+            export CARGO_TARGET_X86_64_LINUX_ANDROID_AR="$android_tools/llvm-ar"
             export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
-            export PATH="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin/":$PATH
             mkdir -p "$BASE/../../jniLibs/x86_64/"
-			case $mode in
-				'release')
-					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl" --release
-					cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
-					;;
-				*)
-					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl"
-					cp "$BASE/target/$target/debug/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
-					;;
-			esac
+			cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf/default-ring" --release
+			cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/x86_64/"
             ;;
         'aarch64-linux-android')
             export CC_aarch64_linux_android="$android_tools/${target}${api}-clang"
-            export AR_aarch64_linux_android="$android_tools/${target}-ar"
-            export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$android_tools/$target-ar"
+            export AR_aarch64_linux_android="$android_tools/llvm-ar"
+            export CARGO_TARGET_AARCH64_LINUX_ANDROID_AR="$android_tools/llvm-ar"
             export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
-            export PATH="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin/":$PATH
             mkdir -p "$BASE/../../jniLibs/arm64-v8a/"
-			case $mode in
-				'release')
-					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl" --release
-					cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
-					;;
-				*)
-					cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf-mobile/default-openssl"
-					cp "$BASE/target/$target/debug/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
-					;;
-			esac
+			cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf/default-ring" --release
+			cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/arm64-v8a/"
 			;;
+        'armv7-linux-androideabi')
+            export CC_armv7_linux_androideabi="$android_tools/armv7a-linux-androideabi${api}-clang"
+            export AR_armv7_linux_androideabi="$android_tools/llvm-ar"
+            export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_AR="$android_tools/llvm-ar"
+            export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="$android_tools/armv7a-linux-androideabi${api}-clang"
+            mkdir -p "$BASE/../../jniLibs/armeabi-v7a/"
+			cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf/default-ring" --release
+			cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/armeabi-v7a/"
+			;;
+        'i686-linux-android')
+            export CC_i686_linux_android="$android_tools/${target}${api}-clang"
+            export AR_i686_linux_android="$android_tools/llvm-ar"
+            export CARGO_TARGET_I686_LINUX_ANDROID_AR="$android_tools/llvm-ar"
+            export CARGO_TARGET_I686_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
+            mkdir -p "$BASE/../../jniLibs/x86/"
+			cargo build --target $target --manifest-path "$BASE/Cargo.toml" --no-default-features --features "leaf/default-ring" --release
+			cp "$BASE/target/$target/release/libleafandroid.so" "$BASE/../../jniLibs/x86/"
+            ;;
         *)
             echo "Unknown target $target"
             ;;
--- app/src/main/rust/leaf-android/src/lib.rs
+++ app/src/main/rust/leaf-android/src/lib.rs
@@ -5,14 +5,38 @@ use jni::{
 
 #[allow(non_snake_case)]
 #[no_mangle]
-pub unsafe extern "C" fn Java_com_leaf_example_aleaf_SimpleVpnService_runLeaf(
+pub unsafe extern "C" fn Java_com_im_im_1lib_LeafVpnService_runLeaf(
     env: JNIEnv,
     _: JClass,
-    configPath: JString,
-    protect_path: JString,
+    config_path: JString,
 ) {
-    leaf_mobile::run_leaf(
-        env.get_string(configPath).expect("invalid path").as_ptr(),
-        env.get_string(protect_path).expect("invalid path").as_ptr(),
-    );
+    let config_path = env
+        .get_string(config_path)
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_owned();
+    let opts = leaf::StartOptions {
+        config: leaf::Config::File(config_path),
+        runtime_opt: leaf::RuntimeOption::SingleThread,
+    };
+    leaf::start(1, opts).unwrap();
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub unsafe extern "C" fn Java_com_im_im_1lib_LeafVpnService_stopLeaf(
+    _: JNIEnv,
+    _: JClass,
+) {
+    leaf::shutdown(1);
+}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub unsafe extern "C" fn Java_com_im_im_1lib_LeafVpnService_leafIsRunning(
+    _: JNIEnv,
+    _: JClass,
+) {
+    leaf::is_running(1);
 }
